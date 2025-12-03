7bb2d5982ea98d2346b700746f97b0622efaf272
Al-Bara <albarashazlee174@gmail.com>
2025-12-03 11:35:24 +0000

    login + register page added

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,12 +1,27 @@
-<h1>Login</h1>
+<div class="auth-container">
+    <div class="auth-box">
 
-<form method="post" action="{{ route('login.login') }}">
-    @csrf
-    <label for="email">Email</label>
-    <input type="text" name="email" id="email" value="{{ old('email') }}">
+    <div class="tab-controls">
+        <button id="login-tab" class="tab active" onclick="showForm('login')">Login</button>
+        <button id="register-tab" class="tab active" onclick="showForm('register')">Register</button>
+    <div>
 
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password">
+    <form id="login-form" method="POST" action="{{route('login.login')}}" class="auth-form active">
+        <h3 class="form-title">Welcome Back</h3>
+        
+        <label for="register-name">Full Name</label>
+        <input type="text" id="register-name" name="name" placeholder="Joe Biden" required>
 
-    <button type="submit">Login</button>
-</form>
+        <label for="register-email">Email Address</label>
+        <input type="email" id="register-email" name="email" placeholder="joe@outlook.com" required>
+
+        <label for="register-password">Password</label>
+        <input type="password" id="register-password" name="password" placeholder="qwerty" required>
+
+        <label for="register-confirm">Confirm Password</label>
+        <input type="password" id="register-confirm" name="password_confirmation" placeholder="qwerty" required>
+
+        <button type="submit" class="submit-btn">Sign Up</button>
+        </form>
+    </div>
+</div>
